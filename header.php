3c3a7acdf6450ca918f486b83d293bfd7d6012cf
spsapa <ytyonti@gmail.com>
2021-05-23 16:02:06 +0000

    first commit

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+ <nav>
+ <label class="logo"><img src="/2d118a0c02f0b65b.gif" alt="아트플랫:메인화면"></label>
+  <ul class="nav_links">
+    <li><a href="/">이용안내<a><li>
+    <li><a href="https://github.com/spsapa/m.comiplatform">Github<a><li>
+    <li><a href="/">고객센터<a><li>
+    <li><a href="/">로그인<a><li>
+    </ul>
+ </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='ko'>
+  <head>
+  	<meta charset="utf-8">
+	  <link rel="preconnect" href="https://fonts.gstatic.com">
+	  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet">
+	  <meta name="viewport" content="width=750px">
+	  <title>아트플랫에 오신것을 환영합니다.</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link href="header.css" rel="stylesheet">
+
+  <script src="https://kit.fontawesome.com/a27819b7d3.js" crossorigin="anonymous"></script>
+</head>
+  <body>
+  <?php include("header.php");?>
+  <div class='conbutton'>
+    <h1>일단 여기에 뭔가를 넣긴 할건데</h1>
+    <h1>그냥 트위터 크롤링이나해서 텍스트 넣어버릴까</h1>
+  </div>
+  </body>
+</html>
